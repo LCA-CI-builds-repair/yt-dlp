5042591b0b6d2b19558357c1e750ec3025e1c599
ubuntu <ubuntu@ip-10-0-0-248>
2024-05-02 11:10:00 +0000

    test_lca_code_editing__code-editing-metrics-xead0287a17953ac__id_53

--- yt_dlp/extractor/extractors.py
+++ yt_dlp/extractor/extractors.py
@@ -11,13 +11,13 @@ if not os.environ.get('YTDLP_NO_LAZY_EXTRACTORS'):
     with contextlib.suppress(ImportError):
         from .lazy_extractors import *  # noqa: F403
         from .lazy_extractors import _ALL_CLASSES
-        _LAZY_LOADER = True
+_LAZY_LOADER = True
 
 if not _LAZY_LOADER:
-    from ._extractors import *  # noqa: F403
+    from . import _extractors  # noqa: F403
     _ALL_CLASSES = [  # noqa: F811
         klass
-        for name, klass in globals().items()
+        for name, klass in _extractors.__dict__.items()
         if name.endswith('IE') and name != 'GenericIE'
     ]
     _ALL_CLASSES.append(GenericIE)  # noqa: F405
