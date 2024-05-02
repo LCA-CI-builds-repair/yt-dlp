75e10f839989aac5e0edf6f3d02e0150cbf563b0
ubuntu <ubuntu@ip-10-0-0-215>
2024-05-02 11:46:24 +0000

    test_lca_code_editing__code-editing-metrics-cdfcab645d0a57a__id_8

--- devscripts/make_issue_template.py
+++ devscripts/make_issue_template.py
@@ -34,12 +34,11 @@ VERBOSE_TMPL = '''
       description: |
         It should start like this:
       placeholder: |
-        [debug] Command-line config: ['-vU', 'https://www.youtube.com/watch?v=BaW_jenozKc']
-        [debug] Encodings: locale cp65001, fs utf-8, pref cp65001, out utf-8, error utf-8, screen utf-8
-        [debug] yt-dlp version nightly@... from yt-dlp/yt-dlp [b634ba742] (win_exe)
-        [debug] Python 3.8.10 (CPython 64bit) - Windows-10-10.0.22000-SP0
-        [debug] exe versions: ffmpeg N-106550-g072101bd52-20220410 (fdk,setts), ffprobe N-106624-g391ce570c8-20220415, phantomjs 2.1.1
-        [debug] Optional libraries: Cryptodome-3.15.0, brotli-1.0.9, certifi-2022.06.15, mutagen-1.45.1, sqlite3-2.6.0, websockets-10.3
+        [debug] Command-line config: ['-v']
+        [debug] Encodings: locale UTF-8, fs utf-8, pref UTF-8, out utf-8 (No ANSI), error utf-8 (No ANSI), screen utf-8 (No ANSI)
+        [debug] yt-dlp version stable@2023.11.16 from yt-dlp/yt-dlp [24f827875] (source)
+        [debug] Lazy loading extractors is disabled
+        [debug] Git HEAD: 9953a51
         [debug] Proxy map: {}
         [debug] Request Handlers: urllib, requests
         [debug] Loaded 1893 extractors
