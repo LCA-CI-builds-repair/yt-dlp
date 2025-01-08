dfcb5c4a308b677e43687714859e25326cfa5645
ubuntu <ubuntu@ip-10-0-1-6>
2025-01-08 17:41:42 +0000

    waleko__gpt-4o_1de6__id_52

--- yt_dlp/extractor/mx3.py
+++ yt_dlp/extractor/mx3.py
@@ -79,7 +79,7 @@ class Mx3BaseIE(InfoExtractor):
 
 class Mx3IE(Mx3BaseIE):
     _DOMAIN = 'mx3.ch'
-    _VALID_URL = _Mx3BaseIE._VALID_URL_TMPL % re.escape(_DOMAIN)
+    _VALID_URL = Mx3BaseIE._VALID_URL_TMPL % re.escape(_DOMAIN)
     _TESTS = [{
         'url': 'https://mx3.ch/t/1Cru',
         'md5': '7ba09e9826b4447d4e1ce9d69e0e295f',
@@ -131,7 +131,7 @@ class Mx3IE(Mx3BaseIE):
 
 class Mx3NeoIE(Mx3BaseIE):
     _DOMAIN = 'neo.mx3.ch'
-    _VALID_URL = _Mx3BaseIE._VALID_URL_TMPL % re.escape(_DOMAIN)
+    _VALID_URL = Mx3BaseIE._VALID_URL_TMPL % re.escape(_DOMAIN)
     _TESTS = [{
         'url': 'https://neo.mx3.ch/t/1hpd',
         'md5': '6d9986bbae5cac3296ec8813bf965eb2',
