d55352ce0f4b6afd2acfdb52cc1d8ab7462c2f2e
ubuntu <ubuntu@ip-10-0-1-6>
2025-01-08 17:42:50 +0000

    waleko__gpt-4o_1de6__id_9

--- yt_dlp/downloader/http.py
+++ yt_dlp/downloader/http.py
@@ -26,6 +26,9 @@ from ..utils.networking import HTTPHeaderDict
 
 class HttpFD(FileDownloader):
     def real_download(self, filename, info_dict):
+        if not info_dict.get('url'):
+            raise ValueError("No URL provided for download.")
+
         url = info_dict['url']
         request_data = info_dict.get('request_data', None)
 
