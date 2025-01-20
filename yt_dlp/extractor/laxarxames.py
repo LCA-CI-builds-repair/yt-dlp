557d8b9e452fd07684889aa363d21b9cc03ab4f4
ubuntu <ubuntu@ip-10-0-1-97>
2025-01-20 22:40:51 +0000

    waleko__llama-3.1-405b_9179__id_7

--- yt_dlp/extractor/laxarxames.py
+++ yt_dlp/extractor/laxarxames.py
@@ -34,7 +34,7 @@ class LaXarxaMesIE(InfoExtractor):
                     'Name': 'Mac OS ()',
                 },
             }).encode('utf-8'))
-        )
+        # Assuming there is some code here
 
         if not login['AuthorizationToken']:
             raise Exception('Login failed')
